cf9b89d712c44022e55c97688ac1ab6061dc144c
Xing Lin <linxingnku@gmail.com>
2020-10-09 15:15:32 -0700

    parent wait for child .
    Signed-off-by: Xing Lin <linxingnku@gmail.com>

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include<thread>
 #include<vector>
 #include<mutex>
+#include <condition_variable>
+
 
 class Test {
    int x;
@@ -11,20 +13,37 @@ class Test {
 using namespace std;
 
 std::mutex m;
+condition_variable cv;
+string data = "";
+bool ready=false;
+bool processed=false;
 
 int thread_function(int i) {
-	m.lock();
-	std::cout << "thread " << i << " is running" << std::endl;
-	m.unlock();
+	::unique_lock<mutex> lk(m);
+	while(ready != true)
+		cv.wait(lk);
+	std::cout << "child thread " << i << " is running" << std::endl;
+	data += " + child" + to_string(i);
+
+	processed = true;
+	lk.unlock();
+	cv.notify_one();
 }
 
 int main() {
 	std::cout << "hello world" << std::endl;
 
-	vector<thread> v;
-	for(int i=0; i < 10; i++)
-		v.emplace_back(thread_function, i);
-	
-	for(thread& t: v)
-		t.join();
+	unique_lock<mutex> lk(m);
+	data += " + parent ";
+	ready = true;
+	lk.unlock();
+	cv.notify_one();
+
+	thread t(thread_function, 1);
+	lk.lock();
+	while(processed != true)
+		cv.wait(lk);
+
+	t.join();
+	std::cout << "data=" << data << std::endl;
 }
