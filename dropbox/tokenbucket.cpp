f0acf6727ecd9273add9c626a03b6e87d68bb51d
Xing Lin <linxingnku@gmail.com>
2020-11-03 19:35:33 -0800

    add tokenbucket.cpp
    Signed-off-by: Xing Lin <linxingnku@gmail.com>

--- dropbox/tokenbucket.cpp
+++ dropbox/tokenbucket.cpp
@@ -12,6 +12,14 @@
 
 using namespace std;
 
+    long gettimems() {
+        struct timeval tp;
+        gettimeofday(&tp, NULL);
+        return (tp.tv_sec * 1000 + tp.tv_usec / 1000);
+    }
+
+long starttime;
+
 class TokenBucket {
     int fillRate;
     int maxToken;
@@ -22,21 +30,18 @@ class TokenBucket {
     queue<int> tokens;
     long lastfilltime;
     bool stop;
+    long sum = 0;
 
 public:
     TokenBucket(int fillRate, int maxToken): fillRate(fillRate), maxToken(maxToken), lastfilltime(-1), stop(false) {}
-    long gettimems() {
-        struct timeval tp;
-        gettimeofday(&tp, NULL);
-        return (tp.tv_sec * 1000 + tp.tv_usec / 1000);
-    }
+
 
     void produce() {
 
         while(true) {
 
             unique_lock<mutex> lk(m);
-            while(tokens.size() >= fillRate && stop == false)
+            while(tokens.size() >= maxToken && stop == false)
                 producer.wait(lk);
 
             if(stop == true)
@@ -45,20 +50,31 @@ public:
             long now = gettimems();
             long toFill = -1;
             if(lastfilltime == -1)
-                toFill = fillRate;
+                toFill = maxToken;
             else 
                 toFill = (now - lastfilltime)/1000.0 * fillRate;
 
-            std::cout << "producer: to fill " << toFill << std::endl;
-            while(tokens.size() < maxToken && toFill > 0) {
-                tokens.push(rand()%maxToken);
-                toFill --;
-            }
-            if(toFill)
-                lastfilltime = now;
+            toFill = ::min((int)toFill, int(maxToken-tokens.size()));
+            
+            
 
+            int count = toFill;
+            while(count > 0) {
+                tokens.push(rand()%maxToken);
+                count --;
+            }
+
+            if(toFill) {
+                sum += toFill;
+                lastfilltime = now;
+                std::cout << "now=" << (now-starttime)/1000 
+                    << ", total token produced: " << sum 
+                    << ", available tokens=" << tokens.size() << std::endl;
+            }
+            
             lk.unlock();
             consumer.notify_all();
+            //std::this_thread::yield();
         }
 
         std::cout << "producer exits\n";
@@ -75,7 +91,7 @@ public:
 
         
             int toFetch = ::min((int)tokens.size(), n);
-            std::cout << "consumer: to fetch " << toFetch << std::endl;
+            //std::cout << "consumer: to fetch " << toFetch << ", available token=" << tokens.size() << std::endl;
             int count = toFetch;
             while(count > 0) {
                 res.push_back(tokens.front());     
@@ -84,25 +100,27 @@ public:
             }
 
             n -= toFetch;
+            lk.unlock();
             producer.notify_all();
-
         }
 
         return std::move(res);
     }
 
     void consumeToken() {
+        std::srand(std::time(0));
 
-        int round = 10;
+        int round = 100;
         while(round) {
-            int n = rand()% 100;
+            int n = rand()% 1000;
             
-            std::cout << "to fetch " << n << " tokens\n"; 
+            std::cout << "consumer to fetch " << n << " tokens\n"; 
             vector<int> tokens = consume(n);
-            for(int t: tokens) 
-                std::cout << t << ",";
+            //for(int t: tokens) 
+            //    std::cout << t << ",";
             std::cout << std::endl;
             round --;
+            std::this_thread::sleep_for(chrono::seconds(1));
         }
 
         unique_lock<mutex> lk(m);
@@ -115,8 +133,9 @@ public:
 
 
 int main() {
+    starttime = gettimems();
 
-    TokenBucket tb(100, 5*100);
+    TokenBucket tb(100, 4*100);
 
     thread producer(&TokenBucket::produce, &tb);
 
